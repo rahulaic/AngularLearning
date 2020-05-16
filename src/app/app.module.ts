f880ac5582dde6171732811d2e48a4bc2d5bed2e
rahul_aic <rahul.agarwal@anaghainfotech.com>
2020-05-16 20:45:18 +0530

    Till Angular Fundamentals in CWM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
+import { AuthorsService } from './authors.service';
+import { CoursesService } from './courses.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
+import { AuthorsComponent } from './authors/authors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    CoursesComponent,
+    AuthorsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CoursesService, AuthorsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
