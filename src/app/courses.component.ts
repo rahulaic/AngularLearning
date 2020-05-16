45a96f6115033597c122f3618a97f690089d9858
rahul_aic <rahul.agarwal@anaghainfotech.com>
2020-05-16 23:45:25 +0530

    built in pipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: ` <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> `,
+  template: ` {{ course.title | uppercase }} <br />
+    {{ course.students | number }} <br />
+    {{ course.rating | number: '1.1-1' }} <br />
+    {{ course.price | currency: 'INR':true:'3.2-2' }} <br />
+    {{ course.releaseDate | date: 'shortDate' }}`,
 })
 export class CoursesComponent {
-  email = 'me@example.com';
-
-  onKeyUp() {
-    console.log(this.email);
-  }
+  course = {
+    title: 'The Complete Angular Course',
+    rating: 4.9745,
+    students: 30132,
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1),
+  };
 }
