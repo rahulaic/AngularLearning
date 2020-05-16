9b2af47c61322119af7b9a46ef9391b3b1f55296
rahul_aic <rahul.agarwal@anaghainfotech.com>
2020-05-16 22:19:00 +0530

    commit till template variables

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,20 +3,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: `
-    <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
-  `,
+  template: ` <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> `,
 })
 export class CoursesComponent {
-  title = 'List of Courses';
-  courses;
+  email = 'me@example.com';
 
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
+  onKeyUp() {
+    console.log(this.email);
   }
 }
