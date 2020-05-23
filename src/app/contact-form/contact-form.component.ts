a6f9a1451e9c7e617735ad87f5a8a3efc0e3795e
rahul_aic <rahul.agarwal@anaghainfotech.com>
2020-05-23 10:55:46 +0530

    template driven form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
   constructor() {}
-
+  onLog(x) {
+    console.log(x);
+  }
+  submit(f) {
+    console.log(f);
+  }
   ngOnInit(): void {}
 }
